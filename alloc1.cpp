4197ecdee6f99d243faa42a251f22a925daeefb5
Dzuy Tran <dktran65@comcast.net>
2019-05-20 16:33:29 -0700

    Update alloc1.cpp with ABV and NDP defects code

--- alloc1.cpp
+++ alloc1.cpp
@@ -10,3 +10,18 @@ int main() {
 	free(a);
 	return 0;
 }
+
+void array_boundery_violation()
+{
+	int aBV[3] = {1,2,3};
+
+	aBV[4]= 5;
+}
+
+
+void null_pointer_deference_general_must()
+{
+	int *iPtr = NULL;
+	*iPtr = 2;
+
+}
