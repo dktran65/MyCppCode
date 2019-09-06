aa355bc29f9981574e8d39da1513363ec32c8731
Dzuy Tran <dktran65@gmail.com>
2019-09-06 09:56:07 -0700

    Update batch file

--- runKwFullCmd.bat
+++ runKwFullCmd.bat
@@ -1,5 +1,6 @@
 set projectName=MyCppCode
 set SVRLOC=192.168.65.129
+REM set SVRLOC=localhost
 
 
 REM *** 1. We should already have CHESS project on Klocwork Server
@@ -12,4 +13,4 @@ REM *** 3. Next Force a full Analysis
 kwbuildproject --url http://%SVRLOC%:8080/%projectName% -o kwtables kwinject.out --force --replace-path %CD%=%projectName%
 
 REM *** 4. we load our result with kwadmin and load to the Klocwork Server
-rem kwadmin --url http://localhost:8080 load %projectName%  kwtables
+REM kwadmin --url http://localhost:8080 load %projectName%  kwtables
