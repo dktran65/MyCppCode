17d4ff54480ff4b153162b3f7beedabc30e31111
Dzuy Tran <dktran65@gmail.com>
2019-09-10 16:06:47 -0700

    Update ReviewTrace script

--- runKwReviewTrace.sh
+++ runKwReviewTrace.sh
@@ -4,14 +4,11 @@ grep ^compile kwinject.out | wc -l
 
 echo ""
 echo "***** Compilers configured:"
-grep ^config kwinject.out | awk -F ';' '{print $2}' | awk -F "|" '{print $1}' | sort -u | tee kwvalidatetrace1.log 
+grep ^config kwinject.out | awk -F ';' '{print $2}' | awk -F "|" '{print $1}' | sort -u | tee kwvalidatetrace1.log
 
 echo ""
-echo "***** Compilers possibly used:"
-grep '"executable":' kwinjectTrace.txt | awk -F',' '{print $4}' | awk -F':' '{print $2}' | sed 's/\"//g' | sort -u > kwvalidatetrace2.log 
-grep -vxF -f kwvalidatetrace1.log kwvalidatetrace2.log 
+echo "***** Compilers possibly used, but not recognized:"
+grep '"executable":' kwinjectTrace.txt| awk -F',' '{print $4}' | awk -F':' '{print $2}' | sed 's/\"//g' | sort -u > kwvalidatetrace2.log
+grep -vxF -f kwvalidatetrace1.log kwvalidatetrace2.log | grep -e "cc" -e "cp" -e "pp" -e "gc" -e "gn" -e "g+" -e "++"
 
 rm -f kwvalidatetrace1.log kwvalidatetrace2.log
-
-
-
